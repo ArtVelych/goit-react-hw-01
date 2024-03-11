e5eba5648880ec57b8b5f338c33b9c79159e364a
ArtVelych <artemvelychko1999@gmail.com>
2024-03-11 20:52:36 +0100

    Task 3 complete

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -4,20 +4,20 @@ const TransactionHistory = ({ items }) => {
   return (
     <table className={css.transactionHistoryTable}>
       <thead className={css.transactionMenuInfo}>
-        <tr className={css.transactionMenuInfoList}>
-          <th>Type</th>
-          <th>Amount</th>
-          <th>Currency</th>
+        <tr>
+          <th className={css.transactionMenuInfoText}>Type</th>
+          <th className={css.transactionMenuInfoText}>Amount</th>
+          <th className={css.transactionMenuInfoText}>Currency</th>
         </tr>
       </thead>
 
       {items.map((item) => {
         return (
-          <tbody key={item.id}>
+          <tbody className={css.transactionMenuValueCells} key={item.id}>
             <tr>
-              <td>{item.type}</td>
-              <td>{item.amount}</td>
-              <td>{item.currency}</td>
+              <td className={css.transactionMenuValue}>{item.type}</td>
+              <td className={css.transactionMenuValue}>{item.amount}</td>
+              <td className={css.transactionMenuValue}>{item.currency}</td>
             </tr>
           </tbody>
         );
